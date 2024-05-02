20dde4f4caffdd40d5231064a10690f3d0cd223a
Isaac Lavana <isaaclavana@gmail.com>
2024-05-01 20:56:41 -0500

    hi

--- TestProject2/obj/Debug/net8.0/TestProject2.AssemblyInfo.cs
+++ TestProject2/obj/Debug/net8.0/TestProject2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e23c7e03810b59c100012dbaf3c5b5992ed7466a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9954cfd929c50a95cd22632460de3807e7174ea2")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
